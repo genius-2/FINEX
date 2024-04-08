3b71a47e70e498846562b0dfd47001d6c356683a
dakha21 <damir11hasanshin@gmail.com>
2024-04-08 21:29:15 +0300

    added Programm.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите строку разделяя слова пробелами");
+string s = Console.ReadLine();
+string s1;
+int a = 0;
+while (s != null)
+{
+    a = s.IndexOf(" ");
+    if (a == -1 && s.Length > 3)
+    {
+        break;
+    }
+    else if (a == -1 && s.Length <= 3)
+    {
+        Console.WriteLine(s);
+        break;
+    }
+    else if (a <= 3)
+    {
+        s1 = s.Substring(0, a);
+        Console.WriteLine(s1+" ");
+    }
+    s=s.Substring(a + 1);
+}
